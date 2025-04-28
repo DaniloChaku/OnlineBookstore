e24caf3aa7fe2e895e7b32c5c966f46147b8448d
danch <danilochaku@gmail.com>
2025-04-28 10:40:47 +0300

    feat: add entity classes

---
+++ src/OnlineBookstore.Dal/Entities/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineBookstore.Dal.Entities;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public int AuthorId { get; set; }
+    public Author Author { get; set; } = null!;
+    public int GenreId { get; set; }
+    public Genre Genre { get; set; } = null!;
+    public decimal Price { get; set; }
+    public int QuantityAvailable { get; set; }
+}
